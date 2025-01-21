a6fb7fb0d3f3afc0966f0bc467c854337a73889c
hungNv135 <nguyenhungvan135@gmail.com>
2025-01-21 08:21:00 +0700

    Sprint3

---
+++ app/src/main/java/hungnv/ph48907/asm/DeliveryActivity.java
@@ -0,0 +1,3 @@
+package hungnv.ph48907.asm;
+
+public class DeliveryActivity extends AppCompatActivity {
---
+++ app/src/main/java/hungnv/ph48907/asm/Fragment/NotificationsFragment.java
@@ -0,0 +1,21 @@
+package hungnv.ph48907.asm.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.duan1.polyfood.R;
+
+
+public class NotificationsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+}
